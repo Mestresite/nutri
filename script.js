7dc2e290141ac62c5819dd2b0fc024bb6f931593
Mestresite <brunosouzasjjf@gmail.com>
2025-02-11 23:31:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('consultationButton').addEventListener('click', function() {
-    alert('Obrigado por se interessar! Entre em contato para agendar sua consulta gratuita.');
-});
-
