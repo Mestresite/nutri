1f980783be5bbc7abe2e562322bc4dad99e8cfd9
Mestresite <brunosouzasjjf@gmail.com>
2025-02-11 21:53:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('consultationButton').addEventListener('click', function() {
+    alert('Obrigado por se interessar! Entre em contato para agendar sua consulta gratuita.');
+});
+
